b973de68a6e62d6888c99377d9e0ff3b2d923165
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-21 04:29:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+class CashRegister
+  
+  attr_accessor :total, :discount
+  def initialize( discount = 0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(title, price, quantity = 1)
+    total += (price * quantity)
+  end
+  
+  def apply_discount
+    total *= discount
+  end
+end
 
+cr = CashRegister.new(20)
+
+puts cr.discount
