1c676030cb228249c4a03144186a57d6256a0281
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-21 04:37:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,6 +13,8 @@ class CashRegister
   def apply_discount
     
     @total = @total - ( @total * @discount / 100.0 )
+    
+    puts "After the discount, the total comes to $#{@total}."
   end
 end
 
