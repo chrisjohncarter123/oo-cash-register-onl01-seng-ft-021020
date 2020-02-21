9dd00007e4110cb310b889ab8b4c04afcd430f10
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-21 04:38:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
     
     @total = @total - ( @total * @discount / 100.0 )
     
-    puts "After the discount, the total comes to $#{@total}."
+    return "After the discount, the total comes to $#{@total}."
   end
 end
 
